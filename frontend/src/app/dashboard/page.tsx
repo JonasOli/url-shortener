58f5eb001074ae502fcfc8fe8be634d687a411ae
Jonas Oliveira <jonasfissicaro13@gmail.com>
2025-03-11 23:28:53 -0300

    List urls

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,16 +1,67 @@
-'use client';
+import dayjs from 'dayjs';
+import { cookies } from 'next/headers';
 
-import { signOut } from '@/app/actions/auth';
-import { Button } from '@mui/material';
+type Url = {
+  id: number;
+  original: string;
+  short: string;
+  visit_count: number;
+  created_at: string;
+};
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  async function listShortenUrls(): Promise<Url[]> {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get('session-id');
+    const res = await fetch('http://localhost:8000/urls/list', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `${sessionId?.name}=${sessionId?.value}`,
+      },
+      credentials: 'include',
+      cache: 'no-cache',
+    });
+
+    return res.json();
+  }
+
+  const urls = await listShortenUrls();
+
   return (
-    <Button
-      onClick={async () => {
-        await signOut();
-      }}
-    >
-      Logout
-    </Button>
+    <section>
+      <h1>My urls</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Original url</th>
+            <th>Shortened url</th>
+            <th>Visit count</th>
+            <th>Created at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {urls.map((url) => {
+            return (
+              <tr key={url.id}>
+                <td>{url.original.slice(0, 25)}...</td>
+                <td>
+                  <a
+                    rel="stylesheet"
+                    href={`http://localhost:8000/${url.short}`}
+                    target="_blank"
+                  >
+                    {`http://localhost:8000/${url.short}`}
+                  </a>
+                </td>
+                <td>{url.visit_count}</td>
+                <td>{dayjs(url.created_at).format('DD/MM/YYYY HH:mm')}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
   );
 }
