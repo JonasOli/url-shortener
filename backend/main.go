027af613693cd6b5a8a40d88621c7ccb32e4a028
Jonas Oliveira <jonasfissicaro13@gmail.com>
2025-07-15 20:45:03 -0300

    Add cors

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ import (
 	"url-shortener/internal/handlers"
 	"url-shortener/pkg/database"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -29,6 +30,7 @@ func main() {
 	redis := database.InitRedis()
 
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
